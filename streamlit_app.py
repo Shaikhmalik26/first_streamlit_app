da0f5b6998873721146ef08555210d09f5c0f6fe
Shaikhmalik26 <132675446+Shaikhmalik26@users.noreply.github.com>
2023-05-05 18:50:41 +0530

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Sandwitch")
+streamlit.text("Tea")
+streamlit.text("banana")
+streamlit.text("Hard Boiled, free Range Egg")
