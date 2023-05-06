cc3ef423f4b0f942584125efe77abda1f29ba677
Shaikhmalik26 <132675446+Shaikhmalik26@users.noreply.github.com>
2023-05-07 00:37:40 +0530

    Update streamlit_app.py
    fix final error

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,13 @@ if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function= insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
+    
+#Add a button to load a fruit
+if streamlit.button('Get fruit List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)    
   
 
 #dont run anything past here while we troubleshoot
